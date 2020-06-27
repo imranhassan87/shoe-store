b5a84fba08663a555a49fd505bc8825434b90440
imranhassan96 <imranhassan96>
2020-06-27 10:53:48 +0500

    updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import { FaShoppingCart } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 
 const Banner = () => {
-    const basket = useSelector(state => state.basket)
     return (
         <section className="banner-section v-center">
             <div className="shoe-cont" >
                 <h2>The Best Selling Brands Store</h2>
             </div>
+<<<<<<< HEAD
             <Link to='/basket'>
                 <span className="c-icon">{basket.length}</span>
                 <i className="fa icon">&#xf07a;</i>
             </Link>
+=======
+            <Link to='/basket'><FaShoppingCart color="#ccc" size='3rem' style={{ position: 'relative', bottom: 45 }} /></Link>
+>>>>>>> parent of ffc5f17... updated icon
         </section>
     )
 }
