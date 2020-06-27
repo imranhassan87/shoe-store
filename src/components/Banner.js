f7cee0a56cf29bb397470d1e59d09142bf9cd07c
imranhassan96 <imranhassan96>
2020-06-27 10:57:25 +0500

    updated

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,14 +8,11 @@ const Banner = () => {
             <div className="shoe-cont" >
                 <h2>The Best Selling Brands Store</h2>
             </div>
-<<<<<<< HEAD
             <Link to='/basket'>
                 <span className="c-icon">{basket.length}</span>
                 <i className="fa icon">&#xf07a;</i>
             </Link>
-=======
             <Link to='/basket'><FaShoppingCart color="#ccc" size='3rem' style={{ position: 'relative', bottom: 45 }} /></Link>
->>>>>>> parent of ffc5f17... updated icon
         </section>
     )
 }
