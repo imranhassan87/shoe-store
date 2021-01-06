37bff625b412e7929feab79ea95578c451ced480
imranhassan87 <imranhassan926@gmail.com>
2021-01-06 14:29:33 -0800

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Banner = () => {
-    const basket = useSelector(state => state.basket)
-    return (
-        <section className="banner-section v-center">
-            <div className="shoe-cont" >
-                <img src={require('../images/shoe.png')} alt="shoe" />
-            </div>
-            <div className="brand">The Best Selling Brands</div>
-            <Link to='/basket'>
-                <span className="c-icon">{basket.length}</span>
-                <i className="fa icon-basket">&#xf07a;</i>
-            </Link>
-        </section>
-    )
-}
+  const basket = useSelector((state) => state.basket);
+  return (
+    <section className="banner-section v-center">
+      <div className="shoe-cont">
+        <img src={require("../images/shoe.png")} alt="shoe" />
+      </div>
+      <div className="brand">The Best Selling Brands of shoes</div>
+      <Link to="/basket">
+        <span className="c-icon">{basket.length}</span>
+        <i className="fa icon-basket">&#xf07a;</i>
+      </Link>
+    </section>
+  );
+};
 
-export default Banner
+export default Banner;
